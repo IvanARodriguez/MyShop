7a25d425d6d8ce74ece45cab8d77bc77c53c32de
Ivan A Rodriguez <ivancalvo91@gmail.com>
2024-07-07 13:24:53 -0400

    Initial commit practice Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, HeaderComponent],
 })
-export class AppComponent {
-  title = 'MyShop';
-}
+export class AppComponent {}
